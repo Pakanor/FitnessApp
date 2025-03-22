6f5f1ae5f4fc56286ace614a17cd99dd1c554310
= <=>
2025-03-22 17:50:19 +0100

    worked MVC

--- Programowanie/MainWindow.xaml.cs
+++ Programowanie/MainWindow.xaml.cs
@@ -19,33 +19,31 @@ using System.Windows.Threading;
 using Programowanie.Services;
 using Programowanie.Interfaces;
 using Programowanie.ViewModels;
+using Programowanie.Controllers;
+using Programowanie.Helpers;
 
 namespace Programowanie
 {
 
     public partial class MainWindow : Window
     {
-
         private ICameraService _cameraService; // Zmienna przechowująca instancję serwisu kamery
-
-        private BarcodeReader<Bitmap> barcodeReader;      // Czytnik kodów kreskowych
-        private ProductServiceAPI _productService;
-        private bool isBarcodeScanned = false;
         private MainViewModel _viewModel;
         private BarcodeReaderService _barcodeReaderService;
-        
+        private readonly MainWindowController _controller;
 
         public MainWindow()
         {
             InitializeComponent();
-            _productService = new ProductServiceAPI();
             _viewModel = new MainViewModel();
             DataContext = _viewModel;
             _cameraService = new CameraService();
             _cameraService.FrameReceived += CameraService_FrameReceived;
             _barcodeReaderService = new BarcodeReaderService();
             _barcodeReaderService.BarcodeDetected += OnBarcodeDetected;
+            _controller = new MainWindowController(this, _viewModel);
         }
+
         private void CameraService_FrameReceived(object sender, System.Drawing.Bitmap e)
         {
             // Kiedy otrzymujemy nową klatkę, aktualizujemy interfejs użytkownika
@@ -55,132 +53,50 @@ namespace Programowanie
                 _barcodeReaderService.DecodeBarcode(e); // Wywołanie skanowania
             });
         }
+
         private async void OnBarcodeDetected(object sender, string barcode)
         {
-            _ = Dispatcher.Invoke(async () =>
+            // Wywołanie po wykryciu kodu kreskowego
+            await Dispatcher.Invoke(async () =>
             {
                 BarcodeResult.Text = "Kod: " + barcode;
                 await _viewModel.LoadProductByBarcode(barcode);
-                _cameraService.StopCamera();  
+                _cameraService.StopCamera();  // Zatrzymanie kamery
             });
         }
 
+        // Przycisk uruchamiający skanowanie
         private void StartScanning_Click(object sender, RoutedEventArgs e)
         {
-            _cameraService.StartCamera();
-            ResetUI("scan");
-            isBarcodeScanned = false;
-
-        }
-        public class Debouncer
-        {
-            private DispatcherTimer _timer;
-            private Action _action;
-
-            public Debouncer(int milliseconds)
-            {
-                _timer = new DispatcherTimer
-                {
-                    Interval = TimeSpan.FromMilliseconds(milliseconds)
-                };
-                _timer.Tick += (s, e) =>
-                {
-                    _timer.Stop();
-                    _action?.Invoke();
-                };
-            }
-
-            public void Debounce(Action action)
-            {
-                _action = action;
-                _timer.Stop();
-                _timer.Start();
-            }
+            _controller.StartScanning();  // Wywołanie logiki skanowania w kontrolerze
         }
 
-
-
-        //wzorce projektowe-----------------------------------------------------------------
-
-
-
-        private readonly Debouncer _debouncer = new Debouncer(500); // Opóźnienie 500ms
-
+        // Obsługuje zmiany w polu tekstowym
         private async void ProductName_TextChanged(object sender, TextChangedEventArgs e)
         {
-            ProductNameChange.Visibility = Visibility.Visible;
-            ProductName.Text = "Wpisujesz: " + ProductNameChange.Text;
-            _debouncer.Debounce(async () =>
-            {
-                string productName = ProductNameChange.Text.Trim();
-                if (productName.Length > 3)
-                {
-                    _ = _viewModel.LoadProductByName(productName);
-
-                }
-            });
-
-
-
+            _controller.OnProductNameChanged();  // Zlecenie obsługi w kontrolerze
         }
 
         private void AddProduct_Click(object sender, RoutedEventArgs e)
         {
-            ResetUI("add_product");
-            _cameraService.StopCamera();
+            _controller.AddProduct();  // Dodanie produktu
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            ResetUI("start");
+            _controller.BackToStart();  // Powrót do ekranu startowego
         }
 
-
-        private void ResetUI(string Int)
-        {
-            if (Int == "add_product")
-            {
-                startPanel.Visibility = Visibility.Collapsed;
-                InputPanel.Visibility = Visibility.Visible;
-                BackButton.Visibility = Visibility.Visible;
-                BarcodeResult.Text = "";
-                CameraPreview.Visibility = Visibility.Collapsed;
-
-
-
-            }
-            else if (Int == "start")
-            {
-
-                startPanel.Visibility = Visibility.Visible;
-                InputPanel.Visibility = Visibility.Collapsed;
-                BackButton.Visibility = Visibility.Collapsed;
-
-                BarcodeResult.Text = "";
-
-
-            }
-            else if (Int == "scan")
-            {
-                CameraPreview.Visibility = Visibility.Visible;
-                BarcodeResult.Text = "";
-
-            }
-
-
-        }
-
-
-
+        // Zatrzymanie kamery przy zamknięciu aplikacji
         protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
         {
             base.OnClosing(e);
-
-            // Zatrzymaj kamerę, jeśli jest aktywna
             if (_cameraService != null)
             {
-                _cameraService.StopCamera();
+                _cameraService.StopCamera();  // Zatrzymaj kamerę, jeśli jest aktywna
             }
         }
     }
+
+
 }
