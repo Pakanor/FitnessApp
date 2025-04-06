f16a3e0f9b3a4caadae65722338b7350244ed157
= <=>
2025-04-06 14:00:11 +0200

    showing added products

---
+++ Programowanie/Services/ProductsCatalogeService.cs
@@ -0,0 +1,30 @@
+﻿using FitnessApp.DataAccess;
+using FitnessApp.Interfaces;
+using FitnessApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Services
+{
+    internal class ProductsCatalogeService : IProductsCatalogeService
+    {
+        private readonly ProductLogRepository _logRepository;
+        public ProductsCatalogeService(ProductLogRepository logRepository)
+        {
+            _logRepository = logRepository;
+        }
+        public async Task<List<ProductLogEntry>> GetRecentLogsAsync()
+        {
+            return await _logRepository.GetAllAsync();
+        }
+
+        public async Task<bool> HasAnyLogsAsync()
+        {
+            var logs = await _logRepository.GetAllAsync();
+            return logs.Any();
+        }
+    }
+}
