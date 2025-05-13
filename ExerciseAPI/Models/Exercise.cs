b58ab6fabf3fbef1929001e7bfa7609c7ba37880
= <=>
2025-05-13 20:30:16 +0200

    working on new API imported data from ExerciseDB

---
+++ ExerciseAPI/Models/Exercise.cs
@@ -0,0 +1,13 @@
+﻿namespace ExerciseAPI.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string ExternalId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? GifUrl { get; set; }  // Nowe pole na URL do gifa
+    }
+}
---
+++ ExerciseAPI/Program.cs
@@ -0,0 +1,31 @@
+using ExerciseAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql("Host=localhost;Database=exercise;Username=postgres;Password=Pakan135@"));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<ExerciseDbImportService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
