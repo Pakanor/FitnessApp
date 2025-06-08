35450de05fc1b30b16b46142476dc8c8c33583a9
= <=>
2025-06-08 18:58:13 +0200

    change

--- BackendLogicApi/Services/CalorieCalculatorService.cs
+++ BackendLogicApi/Services/CalorieCalculatorService.cs
@@ -17,15 +17,17 @@ namespace BackendLogicApi.Services
                 return null;
 
             double factor = grams / 100.0;
+
             return new Nutriments
             {
-                Energy = product.Nutriments.Energy * factor,
-                Fat = product.Nutriments.Fat * factor,
-                Carbs = product.Nutriments.Carbs * factor,
-                Proteins = product.Nutriments.Proteins * factor,
-                Salt = product.Nutriments.Salt * factor,
+                Energy = Math.Round(product.Nutriments.Energy * factor, 2),
+                Fat = Math.Round(product.Nutriments.Fat * factor, 2),
+                Carbs = Math.Round(product.Nutriments.Carbs * factor, 2),
+                Proteins = Math.Round(product.Nutriments.Proteins * factor, 2),
+                Salt = Math.Round(product.Nutriments.Salt * factor, 2),
                 EnergyUnit = product.Nutriments.EnergyUnit
             };
         }
+
     }
 }
